a196e2fce3dfd3e1a261f812506cfe4ea879e02e
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-11-24 15:49:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 2;
+
+let num3 = 12;
+let num4 = 48;
+
+let mod = num4 / num3;
+
+let max = Math.max(5,10,15,20);
